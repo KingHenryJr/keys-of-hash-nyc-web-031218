14ee5d22cc878106fd7c526a7ce6bca3fd18a4b6
Henry Zhang <henryyihengzhang@gmail.com>
2018-03-01 18:30:21 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
     self.each do |key, value|
       arguments.each do |argument_all|
         if argument_all == value
-        ans << value
+          ans << value
         end 
       end
     end
